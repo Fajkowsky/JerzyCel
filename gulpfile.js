713d41eb51e799f111327d186ad5d78f03b0acd3
Fajkowsky <dawidfajkowski@gmail.com>
2017-01-27 23:33:51 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp'),
+    del = require("del"),
+    pump = require('pump'),
+    htmlmin = require('gulp-htmlmin'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+var paths = {
+    html: "src/*.html",
+    sass: "src/static/sass/*.scss",
+    js: "src/static/js/*.js",
+    dist: "build"
+};
+
+var names = {
+    js: "out.js",
+    css: "out.css"
+};
+
+gulp.task("html", function () {
+    return pump([
+        gulp.src(paths.html),
+        htmlmin({collapseWhitespace: true}),
+        gulp.dest(paths.dist)
+    ]);
+});
+
+gulp.task("sass", function () {
+    var config = {
+        includePaths: ['./bower_components/bulma/'],
+        outputStyle: 'compressed'
+    };
+
+    return pump([
+        gulp.src(paths.sass),
+        sass(config),
+        concat(names.css),
+        gulp.dest(paths.dist)
+    ]);
+});
+
+gulp.task("js", function () {
+    return pump([
+        gulp.src(paths.js),
+        uglify(),
+        concat(names.js),
+        gulp.dest(paths.dist)
+    ]);
+});
+
+gulp.task("clean", function () {
+    return del([paths.dist]);
+});
+
+gulp.task("watch", function () {
+    gulp.watch(paths.html, ["html"]);
+    gulp.watch(paths.sass, ["sass"]);
+    gulp.watch(paths.js, ["js"]);
+});
+
+gulp.task("default", ["clean", "html", "sass", "js", "watch"]);
