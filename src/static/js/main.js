a7129aee6c5b1bb0489b46381f527604646f4cfe
Piotr Ciastko <ciaasteczkowy@gmail.com>
2017-04-19 23:42:20 +0200

    scsss colors and navbar

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,10 @@
 (function () {
-    "use strict";
+  "use strict";
+
+  const navMenu = document.querySelector('.nav-menu');
+  const navToggle = document.querySelector('.nav-toggle');
+  navToggle.addEventListener('click', function () {
+    navMenu.classList.toggle('is-active');
+    navToggle.classList.toggle('is-active');
+  });
 })();
