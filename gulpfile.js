f95ded3cd19d824df3679c7c8b9ff56b4f1e1217
Fajkowsky <dawidfajkowski@gmail.com>
2017-04-24 18:03:27 +0200

    get rid of uncss

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,77 @@
 var gulp = require('gulp'),
-    del = require("del"),
-    pump = require('pump'),
-    htmlmin = require('gulp-htmlmin'),
-    sass = require('gulp-sass'),
-    uglify = require('gulp-uglify'),
-    cssnano = require('gulp-cssnano'),
-    imagemin = require('gulp-imagemin'),
-    concat = require('gulp-concat'),
-    nunjucksRender = require('gulp-nunjucks-render'),
-    uncss = require('gulp-uncss');
+  del = require("del"),
+  pump = require('pump'),
+  htmlmin = require('gulp-htmlmin'),
+  sass = require('gulp-sass'),
+  uglify = require('gulp-uglify'),
+  cssnano = require('gulp-cssnano'),
+  imagemin = require('gulp-imagemin'),
+  concat = require('gulp-concat'),
+  nunjucksRender = require('gulp-nunjucks-render');
 
 var paths = {
-    html: "src/pages/*.html",
-    sass: "src/static/sass/*.scss",
-    js: "src/static/js/*.js",
-    img: "src/static/img/*",
-    dist: "build/",
-    ftp: "/public_html"
+  html: "src/pages/*.html",
+  sass: "src/static/sass/*.scss",
+  js: "src/static/js/*.js",
+  img: "src/static/img/*",
+  dist: "build/",
+  ftp: "/public_html"
 };
 
 var names = {
-    js: "out.js",
-    css: "out.css"
+  js: "out.js",
+  css: "out.css"
 };
 
 gulp.task("html", function () {
-    return pump([
-        gulp.src(paths.html),
-        nunjucksRender({path: ['src/templates/']}),
-        htmlmin({collapseWhitespace: true}),
-        gulp.dest(paths.dist)
-    ]);
+  return pump([
+    gulp.src(paths.html),
+    nunjucksRender({path: ['src/templates/']}),
+    htmlmin({collapseWhitespace: true}),
+    gulp.dest(paths.dist)
+  ]);
 });
 
 gulp.task("sass", function () {
-    var config = {
-        includePaths: ['./bower_components/bulma/', './node_modules/font-awesome/scss']
-    };
+  var config = {
+    includePaths: ['./bower_components/bulma/', './node_modules/font-awesome/scss']
+  };
 
-    return pump([
-        gulp.src(paths.sass),
-        sass(config),
-        concat(names.css),
-        uncss({html: ['src/**/*.html']}),
-        cssnano(),
-        gulp.dest(paths.dist)
-    ]);
+  return pump([
+    gulp.src(paths.sass),
+    sass(config),
+    concat(names.css),
+    cssnano(),
+    gulp.dest(paths.dist)
+  ]);
 });
 
 gulp.task("js", function () {
-    return pump([
-        gulp.src(paths.js),
-        uglify({compress: {unused: false}}),
-        concat(names.js),
-        gulp.dest(paths.dist)
-    ]);
+  return pump([
+    gulp.src(paths.js),
+    uglify({compress: {unused: false}}),
+    concat(names.js),
+    gulp.dest(paths.dist)
+  ]);
 });
 
-gulp.task("img", function() {
-   return pump([
-       gulp.src(paths.img),
-       imagemin(),
-       gulp.dest(paths.dist + "/img")
-   ]);
+gulp.task("img", function () {
+  return pump([
+    gulp.src(paths.img),
+    imagemin(),
+    gulp.dest(paths.dist + "/img")
+  ]);
 });
 
 gulp.task("clean", function () {
-    return del([paths.dist]);
+  return del([paths.dist]);
 });
 
 gulp.task("watch", function () {
-    gulp.watch(paths.html, ["html"]);
-    gulp.watch(paths.sass, ["sass"]);
-    gulp.watch(paths.js, ["js"]);
-    gulp.watch(paths.img, ["img"]);
+  gulp.watch(paths.html, ["html"]);
+  gulp.watch(paths.sass, ["sass"]);
+  gulp.watch(paths.js, ["js"]);
+  gulp.watch(paths.img, ["img"]);
 });
 
 gulp.task("build", ["html", "sass", "js", "img"]);
